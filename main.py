f2849ac3bf2f9efc9e1d9e8a50a7ca1ed8146ab5
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2022-06-11 20:23:15 +0000

    Add hack-story with if-else logic

--- main.py
+++ main.py
@@ -22,3 +22,21 @@ print('''
 ''')
 print("Welcome to Hacker role play.\n")
 print("Your mission is to hack this computer before cybersecurity team found you.") 
+
+choice1 = input('You\'re in terminal. What choice you want to choose (hack in that dir or change to another directory)? Type "hack" or "cd" \n').lower()
+if choice1 == "cd":
+  choice2 = input('You\'ve come to the right directory. You need to choose 1 of 2 approach to hack. Type "virus" to spread instantly. Type "malware" to insert dangerous software. \n').lower()
+  if choice2 == "malware":
+    choice3 = input("Perfect choice! let finish by choose an appropriate malware. One spyware, one ransomware and one worms. Which type do you choose? \n").lower()
+    if choice3 == "worms":
+      print("Inappropriate malware, you got caught. Game Over.")
+    elif choice3 == "ransomware":
+      print("This computer is on your control! You Win!")
+    elif choice3 == "spyware":
+      print("Inappropriate malware, you got caught. Game Over.")
+    else:
+      print("You didn't choose any malware, out of time. Game Over.")
+  else:
+    print("Wrong approach! Game Over.")
+else:
+  print("You got caught by security team. Game Over.")
